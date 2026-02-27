42babe22c826542ca7653cfbec65c9e55f6972fd
hasinamusadiq <hasinamusadiq@gmail.com>
2026-02-27 19:56:33 +0430

    Create __init__.py

---
+++ src/__init__.py
@@ -0,0 +1,11 @@
+"""
+Maseer Automation - AI Video Marketing for Afghan Businesses
+"""
+
+__version__ = "1.0.0"
+__author__ = "Maseer Team"
+__description__ = "Automated video reel generation for Kabul brands"
+
+from . import ai_engine
+from . import image_service
+from . import video_creator
